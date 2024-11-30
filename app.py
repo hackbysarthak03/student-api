f18bbb4eb5e4efdd7417224e0a656b29a3214df6
Sarthak Vishwakarma <vsarthak62@gmail.com>
2024-12-01 02:41:56 +0530

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from Routes.routes import routes  # Import renamed router
+import uvicorn
+import os
 
 app = FastAPI()
 
@@ -9,3 +11,8 @@ app.include_router(routes, prefix="/api", tags=["Students"])
 @app.get('/')
 async def root():
     return {"message": "Welcome to the API"}
+
+if __name__ == "__main__":
+    
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
